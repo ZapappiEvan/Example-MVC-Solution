a3e7ea7fcc7c82ae91b79bb7abd569c0a602e3e7
EvanHarvey <evan.harvey@zapappi.com>
2018-09-14 11:25:26 +0100

    Add project files.

---
+++ Example MVC Solution/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Rest;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zapappi.Client;
+
+namespace Example_MVC_Solution.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected string SubsId = ConfigurationManager.AppSettings["ZapappiSubscriptionId"];
+
+        protected ZapappiClient client = new ZapappiClient(new Uri("https://zapappi-publicapi-dev-test.azurewebsites.net"), new BasicAuthenticationCredentials()
+        {
+            UserName = ConfigurationManager.AppSettings["ZapappiAPIKey"],
+            Password = ConfigurationManager.AppSettings["ZapappiAPISecret"]
+        });
+    }
+}
---
+++ Example MVC Solution/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zapappi.Client;
+using Zapappi.Client.Models;
+
+namespace Example_MVC_Solution.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public ViewResult Index()
+        {
+            //Bring back a list of numbers
+            var AllNumbers = GetExistingNumbers();
+
+            return View(AllNumbers);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public List<TelephoneNumberModel> GetExistingNumbers() 
+        {
+            return client.Numbers.ExistingNumbers(SubsId).Items.ToList();
+        }
+    }
+}
